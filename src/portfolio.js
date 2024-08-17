a6284b0bb5d40ba32abde45fcbe243e21f277970
Aditya Oli <adityaoligehu@gmail.com>
2024-08-17 18:27:45 +1200

    Added Schooling

--- src/portfolio.js
+++ src/portfolio.js
@@ -147,20 +147,20 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: "Harvard University",
-      logo: require("./assets/images/westpac.png"),
-      subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
-      desc: "Participated in the research of XXX and published 3 papers.",
+      schoolName: "Graphic Era Hill University",
+      logo: require("./assets/images/gehu.png"),
+      subHeader: "Bachelor of Technology in Computer Science & Engineering",
+      duration: "August 2012 - July 2016",
+      desc: "Completed Engineering with Honors.",
       descBullets: [
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
       ]
     },
     {
-      schoolName: "Stanford University",
-      logo: require("./assets/images/westpac.png"),
-      subHeader: "Bachelor of Science in Computer Science",
+      schoolName: "Countrywide Public School",
+      logo: require("./assets/images/cps.png"),
+      subHeader: "",
       duration: "September 2013 - April 2017",
       desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
       descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
