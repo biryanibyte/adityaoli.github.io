5e4273e493a9ad480ebaa773d32b0ecead10d3bf
Aditya Oli <adityaoligehu@gmail.com>
2024-08-26 19:19:29 +1200

    Fix Twitter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -52,7 +52,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href=""
+                href="https://www.linkedin.com/in/adioli/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
